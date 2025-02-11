6f0b3113f5868875e6cee39e978e2622cb797289
Igor Tarelkin <igor.tarelkin@adastragrp.com>
2025-02-11 14:59:00 +0100

    fix: adaptiv ui for this page

--- src/pages/Users/UsersList.tsx
+++ src/pages/Users/UsersList.tsx
@@ -3,44 +3,51 @@ import { useQuery } from '@tanstack/react-query';
 import { getUsers } from '../../api/usersApi';
 
 export const UsersList = () => {
-  const {
-    data: users,
-    isLoading,
-    error,
-    isFetched,
-  } = useQuery({
-    queryKey: ['users'],
-    queryFn: getUsers,
-  });
+  const { data: users, isLoading, error } = useQuery({ queryKey: ['users'], queryFn: getUsers });
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  switch (true) {
+    case isLoading:
+      return <p style={{ textAlign: 'center' }}>Loading...</p>;
+    case !!error:
+      return <p style={{ textAlign: 'center', color: 'red' }}>Error: {error.message}</p>;
+    default:
+      break;
+  }
 
   return (
     <>
       <header style={{ display: 'flex', justifyContent: 'center' }}>
         <h2>Users List</h2>
       </header>
-      {isFetched && (
-        <ul style={{ display: 'grid', gridTemplateColumns: 'repeat(5, minmax(15rem, 1fr))', gap: '1rem' }}>
-          {users?.map((user) => (
-            <li
-              key={user.id}
-              style={{
-                listStyle: 'none',
-                textAlign: 'center',
-                padding: '10px',
-                border: '1px solid #ccc',
-                borderRadius: '8px',
-              }}
-            >
-              <Link to={`/user/${user.id}`} state={{ user }} style={{ textDecoration: 'none', color: 'inherit' }}>
-                <strong>{user.name}</strong>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+      <ul
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          gap: '1rem',
+          padding: '0 2rem',
+        }}
+      >
+        {users?.map((user) => (
+          <li
+            key={user.id}
+            style={{
+              listStyle: 'none',
+              textAlign: 'center',
+              padding: '10px',
+              border: '1px solid #ccc',
+              borderRadius: '8px',
+              minWidth: '15rem',
+              flex: '1 1 calc(20% - 1rem)',
+              maxWidth: '20%',
+            }}
+          >
+            <Link to={`/user/${user.id}`} state={{ user }} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <strong>{user.name}</strong>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
