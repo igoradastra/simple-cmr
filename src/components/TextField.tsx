0cf58b03fb4dd5c8534112e27456a0cfe2bbfa51
Igor Tarelkin <igor.tarelkin@adastragrp.com>
2025-02-12 18:01:16 +0100

    fix: Textfield improvments

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -14,18 +14,21 @@ export const TextField = <T extends FieldValues>({ name, control, label, type }:
   } = useController<T>({
     name,
     control,
-    rules: {
-      required: 'This field is required',
-    },
+    rules: { required: 'This field is required' },
   });
 
   return (
-    <fieldset style={{ border: 'none', margin: '0', padding: '0' }}>
-      <label htmlFor={field.name} style={{ display: 'block', marginLeft: '5px', marginTop: '1rem' }}>
+    <fieldset style={{ border: 'none', margin: 0, padding: 0 }}>
+      <label htmlFor={name} style={{ display: 'block', marginLeft: '0.5rem', marginTop: '1rem' }}>
         {label}:
       </label>
-      <input {...field} id={field.name} type={type} style={{ marginLeft: '5px', height: '20px' }} />
-      {error && <p style={{ color: 'red', margin: '0 0 4px 0' }}>{error.message}</p>}
+      <input
+        {...field}
+        id={name}
+        type={type}
+        style={{ border: '1px solid #858585', borderRadius: '4px', marginLeft: '0.5rem', height: '1.5rem' }}
+      />
+      {error && <p style={{ color: 'red', margin: '0 0 0.5rem 0' }}>{error.message}</p>}
     </fieldset>
   );
 };
