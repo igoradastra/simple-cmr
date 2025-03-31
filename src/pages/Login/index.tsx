fdb1dd4e09c28319e1738e8b422d016ebd48fe12
Igor Tarelkin <igor.tarelkin@adastragrp.com>
2025-03-31 14:25:55 +0200

    LoginPage updated

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,10 @@
 import { Login } from './Login';
-import { GoogleLogin, CredentialResponse } from '@react-oauth/google';
-import {jwtDecode} from 'jwt-decode';
 export const LoginPage = () => {
   return (
     <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <>
-      {/* <section style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}> */}
-        {/* <Login /> */}
-        <GoogleLogin 
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse)
-          if (credentialResponse.credential) {
-            console.log(jwtDecode(credentialResponse.credential))
-          }
-        }}
-
-        onError={()=> console.log('Login error')} />
-      {/* </section> */}
-      </>
+      <section style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+        <Login />        
+      </section>
     </main>
   );
 };
